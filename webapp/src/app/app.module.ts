d66852e43f9edf877fcd28f2c24ee0b43d6043d6
Nicolas Crappe <nicolas@email.com>
2020-01-12 21:41:58 +0100

    add first angular version

---
+++ webapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { EventComponent } from './event/event.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
